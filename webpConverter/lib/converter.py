8568737b9247a98f179b33629e156dd215074a0e
Alessio Michelini <amichelini@hubspot.com>
2020-02-17 09:59:41 +0000

    Refactored code

--- webpConverter/lib/converter.py
+++ webpConverter/lib/converter.py
@@ -1,34 +1,34 @@
 from PIL import Image
-from lib.formats import Formats
+from enum import Enum
 import os
 
-class FormatException(Exception):
-    """Format not recognized"""
-    pass
+class Formats(Enum):
+    jpg = 'jpg'
+    jpeg = 'jpg'
+    png = 'png'
+    webp = 'webp'
 
 def convertImage(source, file_format, quality = 100):
     try:
         path = os.path.split(source)
         filepath = os.path.splitext(path[1])
+        print("Converting {image} to {file_format}".format(image=path[1], file_format=file_format))
         source_path = path[0]
         file_name = filepath[0]
         file_extension = filepath[1][1:]
-        if Formats[file_format.upper()] is None:
+        if Formats[file_format] is None:
             raise KeyError
-        dest_extension = Formats[file_format.upper()].value
-        print(dest_extension, file_extension)
+        dest_extension = Formats[file_format].value
         if dest_extension == file_extension:
             raise Exception('Same extension, please choose another format')
-        print(file_format)
         dest_file = "{}/{}.{}".format(source_path, file_name, dest_extension)
-        print(dest_file)
         if os.path.exists(dest_file):
             raise Exception('File {} already exists'.format(dest_file))
         im = Image.open(source)
-        im.save(dest_file, format, quality = quality)
+        im.save(dest_file, file_format, quality = quality)
     except KeyError as error:
         print('File format not recognized')
     except Exception as error:
         print(error)
     finally:
-        print("All done!")
+        print("Your image has been converted to {file_format}!".format(file_format=file_format))
