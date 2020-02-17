23cd056e3813830e686d822620e1bba9fab9aee8
Alessio Michelini <amichelini@hubspot.com>
2020-02-17 09:17:53 +0000

    Improved error handling

--- lib/converter.py
+++ lib/converter.py
@@ -12,10 +12,11 @@ def convertImage(source, file_format, quality = 100):
         filepath = os.path.splitext(path[1])
         source_path = path[0]
         file_name = filepath[0]
-        file_extension = filepath[1]
+        file_extension = filepath[1][1:]
         if Formats[file_format.upper()] is None:
-            raise Exception('File format not recognized')
+            raise KeyError
         dest_extension = Formats[file_format.upper()].value
+        print(dest_extension, file_extension)
         if dest_extension == file_extension:
             raise Exception('Same extension, please choose another format')
         print(file_format)
@@ -25,7 +26,9 @@ def convertImage(source, file_format, quality = 100):
             raise Exception('File {} already exists'.format(dest_file))
         im = Image.open(source)
         im.save(dest_file, format, quality = quality)
+    except KeyError as error:
+        print('File format not recognized')
     except Exception as error:
-        print("Something went wrong", error)
+        print(error)
     finally:
         print("All done!")
